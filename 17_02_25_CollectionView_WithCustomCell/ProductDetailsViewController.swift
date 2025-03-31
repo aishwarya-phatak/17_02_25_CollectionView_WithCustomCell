f132e138a48c708fbf2ac311d584ba27849bdf68
aishwarya <ashp17star@gmail.com>
2025-03-31 16:20:52 +0530

    .

---
+++ 17_02_25_CollectionView_WithCustomCell/ProductDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductDetailsViewController.swift
+//  17_02_25_CollectionView_WithCustomCell
+//
+//  Created by Vishal Jagtap on 31/03/25.
+//
+
+import UIKit
+
+class ProductDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
